59f0c0122c0f483222b0516c18a24afe46faaead
oliviermedor <oliviermedor@yahoo.com>
2019-05-04 08:26:05 -0400

    initialized project

---
+++ index.js
@@ -0,0 +1,10 @@
+// Import a library
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+// Create a component
+const App = () => (
+  <Text> Some Text </Text>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
