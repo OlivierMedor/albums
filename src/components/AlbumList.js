5601854dda9a1ad286d7fe60649788f384625e2e
oliviermedor <oliviermedor@yahoo.com>
2019-05-08 08:23:34 -0400

    added albums list and header

---
+++ src/components/AlbumList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => response.json())
+    .then(responseData => console.log('this is response data', responseData))
+  }
+  render() {
+    return (
+      <View>
+        <Text>Album List</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
